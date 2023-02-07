fc823975517474ec4fdafe4d3522c979e3434039
PetrovicStefan4 <petrovicstefan0404@gmail.com>
2023-02-07 15:40:13 +0100

    changed type of the component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-const Layout: React.FC = ({ children }: any) => {
+const Layout: any = ({ children }: any) => {
   return (
     <>
       <Navbar />
